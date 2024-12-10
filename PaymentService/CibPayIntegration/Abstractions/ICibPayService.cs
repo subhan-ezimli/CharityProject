549d157e2a9af06e3f077c044104416fd20f9110
subhan-ezimli <subhanav@code.edu.az>
2024-12-10 16:56:00 +0400

    ICibPayService interface created

---
+++ PaymentService/CibPayIntegration/Abstractions/ICibPayService.cs
@@ -0,0 +1,18 @@
+﻿using PaymentService.CibPayIntegration.Models.BaseResponse;
+using PaymentService.CibPayIntegration.Models.CreateOrder.Command;
+using PaymentService.CibPayIntegration.Models.CreateOrder.CreateResponse;
+using PaymentService.CibPayIntegration.Models.GetOrder.Requests;
+using PaymentService.CibPayIntegration.Models.GetOrder.Responses;
+using PaymentService.CibPayIntegration.Models.Ping.Responses;
+using PaymentService.CibPayIntegration.Models.RefundOrder.Requests;
+
+namespace PaymentService.CibPayIntegration.Abstractions;
+
+public interface ICibPayService
+{
+    Task<CibBaseResponse<PingResponse>> GetPingResponseAsync();
+    Task<CibBaseResponse<GetPaymentResponse>> GetOrderInfoAsync(string orderId);
+    Task<CibBaseResponse<List<GetPaymentResponse>>> GetOrdersListAsync(GetOrdersRequest query);
+    Task<CreateOrderResponse> CreateOrderAsync(CreateOrderCommand createOrderCommand);
+    Task<CibBaseResponse<List<GetPaymentResponse>>> RefundOrderAsync(RefundOrderRequest refundOrderCommand);
+}
