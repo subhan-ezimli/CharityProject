673bed018c82f1bfd60ece500d11348985ae5d40
subhan-ezimli <subhanav@code.edu.az>
2024-12-11 09:46:20 +0400

    certificated added and some nuget packages added

---
+++ PaymentService/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PaymentService.CibPayIntegration.Implementations;
+
+namespace PaymentService;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCibPayServiceIntegration(this IServiceCollection services)
+    {
+        services.AddScoped<CibPayService>();
+        services.AddHttpClient();
+
+        return services;
+    }
+}
